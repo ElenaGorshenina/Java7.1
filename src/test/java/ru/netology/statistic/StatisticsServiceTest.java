ab6f9d0ac560a6a20c6a1866e36f8323667f8cbc
Elena Gorshenina <elenkagorshenina19.07.14@gmail.com>
2021-07-11 20:48:31 +0300

    goal completed

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
 
-  @Test
+ /** @Test
   void findMax() {
     StatisticsService service = new StatisticsService();
 
@@ -14,6 +14,17 @@ class StatisticsServiceTest {
     long expected = 12;
 
     long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+  }*/
+  @Test
+  void findMin() {
+    StatisticsService service = new StatisticsService();
+
+    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+    long expected = 3;
+
+    long actual = service.findMin(incomesInBillions);
 
     assertEquals(expected, actual);
   }
